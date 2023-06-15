172e6664c18535b00c4f1a3169319d3ffe4aa194
devjaewon <jjazan201@gmail.com>
2023-06-15 14:33:07 +0900

    doc: 3챕터 신경망: 다층 신경망과 행렬곱 활용

--- codes/c3_neutral_network/main.py
+++ codes/c3_neutral_network/main.py
@@ -31,4 +31,34 @@ def draw_chart(activation_fn):
 
 # draw_chart(step_function_using_numpy)
 # draw_chart(sigmoid)
-draw_chart(relu)
+# draw_chart(relu)
+
+x = np.array([[1, 2]])
+w = np.array([[1, 3, 5], [2, 4, 6]])
+y = np.dot(x, w)
+
+print(y)
+
+x = np.array([[1.0, 0.5]])
+
+# 은닉 1층
+w1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+b1 = np.array([[0.1, 0.2, 0.3]])
+a1 = np.dot(x, w1) + b1
+z1 = sigmoid(a1) # 1x3 => 새로운 입력층 (3개 노드)
+
+# 은닉 2층
+w2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+b2 = np.array([[0.1, 0.2]])
+a2 = np.dot(z1, w2) + b2
+z2 = sigmoid(a2) # 1x2 => 새로운 입력층 (2개 노드)
+
+# 은닉 3층
+w3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+b3 = np.array([[0.1, 0.2]])
+a3 = np.dot(z2, w3) + b3
+
+def identity_function(a):
+    print(a)
+
+y = identity_function(a3) # 출력값을 식별함수를 이용해서 결과를 결정한다.
