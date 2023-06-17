80df4288b2960c48152f51355ea694a13b855419
devjaewon <jjazan201@gmail.com>
2023-06-17 17:27:29 +0900

    doc: 3챕터 신경망 : 배치처리

--- book_1/c3_neutral_network/main_mnist.py
+++ book_1/c3_neutral_network/main_mnist.py
@@ -43,14 +43,33 @@ def predict(network, x):
 
     return y
 
-x, t = get_data()
-network = init_network()
+def calc_accuracy_rate():
+    x, t = get_data()
+    network = init_network()
 
-accuracy_cnt = 0
-for i in range(len(x)):
-    y = predict(network, x[i])
-    p = np.argmax(y) # 확률이 가장 높은 원소의 인덱스
-    if p == t[i]:
-        accuracy_cnt += 1
+    accuracy_cnt = 0
+    for i in range(len(x)):
+        y = predict(network, x[i])
+        p = np.argmax(y) # 확률이 가장 높은 원소의 인덱스
+        if p == t[i]:
+            accuracy_cnt += 1
 
-print(accuracy_cnt / len(x))
+    print(float(accuracy_cnt) / len(x))
+
+def calc_accuracy_rate_batch():
+    x, t = get_data()
+    network = init_network()
+
+    batch_size = 100
+    accuracy_cnt = 0
+
+    for i in range(0, len(x), batch_size):
+        x_batch = x[i:i+batch_size]
+        y_batch = predict(network, x_batch)
+        p = np.argmax(y_batch, axis=1)
+        accuracy_cnt += np.sum(p == t[i:i+batch_size])
+
+    print(float(accuracy_cnt) / len(x))
+
+calc_accuracy_rate()
+calc_accuracy_rate_batch()
